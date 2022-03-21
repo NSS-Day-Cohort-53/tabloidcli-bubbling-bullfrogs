8ca9a4fffbe58cc3316355b2ee8b670bc1a1f122
Bryce Borba <borbaesb@gmail.com>
2022-03-21 12:52:37 -0700

    User may now delete blog entries

--- TabloidCLI/Repositories/BlogRepository.cs
+++ TabloidCLI/Repositories/BlogRepository.cs
@@ -74,7 +74,17 @@ namespace TabloidCLI
         }
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"DELETE FROM Blog WHERE id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
         public void Insert(Blog blog)
         {
