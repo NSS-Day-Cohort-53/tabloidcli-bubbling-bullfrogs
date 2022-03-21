50f03f88eef0e503666a043c1a4ce95fc3b093ba
DougSilvera <shortxjdoug@gmail.com>
2022-03-21 13:01:58 -0400

    ticket 44 and 43 complete

---
+++ TabloidCLI/UserInterfaceManagers/JournalManager.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using TabloidCLI.Models;
+
+namespace TabloidCLI.UserInterfaceManagers
+{
+    public class JournalManager : IUserInterfaceManager
+    {
+        private readonly IUserInterfaceManager _parentUI;
+        private JournalRepository _journalRepository;
+        private string _connectionString;
+
+        public JournalManager(IUserInterfaceManager parentUI, string connectionString)
+        {
+            _parentUI = parentUI;
+            _connectionString = connectionString;
+            _journalRepository = new JournalRepository(connectionString);
+        }
+
+        public IUserInterfaceManager Execute()
+        {
+            Console.WriteLine("Journal Menu");
+            Console.WriteLine(" 1) List Journals");
+            Console.WriteLine(" 2) Add Journal Entry");
+            Console.WriteLine(" 0) Go Back");
+
+            Console.Write("> ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    List();
+                    return this;
+                case "2":
+                    Add();
+                    return this;
+                case "0":
+                    return _parentUI;
+                default:
+                    Console.WriteLine("Invalid Selection");
+                    return this;
+            }
+        }
+
+        private void List()
+        {
+            List<Journal> journals = _journalRepository.GetAll();
+            foreach (Journal journal in journals)
+            {
+                Console.WriteLine($"{journal.Id}) {journal.Title} written on {journal.CreateDateTime} \n {journal.Content}");
+            }
+        }
+        private void Add()
+        {
+            Console.WriteLine("New Journal");
+            Journal journal = new Journal();
+
+            Console.Write("Title: ");
+            journal.Title = Console.ReadLine();
+
+            Console.Write("Content: ");
+            journal.Content = Console.ReadLine();
+
+            journal.CreateDateTime = DateTime.Now;
+
+            _journalRepository.Insert(journal);
+        }
+    }
+}
